f98cf3cac3b9fd022b2067abaff1ecf4eb79fece
Phoung monyrak <monyrak0166@gamil.com>
2026-06-13 21:38:54 +0700

    show student info

--- monyrak.cpp
+++ monyrak.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
-
+void showStudentInfo(){
+    string name  ="Phoung monyrak";
+    string major ="Software Engineering";
+}
 int main(){
+    
     return 0;
 }
