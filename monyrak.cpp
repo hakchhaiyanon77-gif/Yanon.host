9dc74a601ce417a3f90ffa6f380ed8768304ffe1
Phoung monyrak <monyrak0166@gamil.com>
2026-06-13 21:36:29 +0700

    Add main funtion

--- monyrak.cpp
+++ monyrak.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void showStudentInfo(){
-    string name  ="Phoung monyrak";
-    string major ="Software Engineering";
-
-    cout << "Student Name : "<< name << endl;
-    cout << "Major        : " << major << endl;
-}
-
 int main(){
-    cout << "===Student Information===" << endl;
-    showStudentInfo();
     return 0;
 }
